a4413b34dc1d1b3549fb063eb277cdb9e478bcfb
--unset <ishwar.rimal@gmail.com>
2024-10-06 08:26:05 +0545

    updated output filename for marketing repo

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -12,6 +12,7 @@ const devConfig = {
   plugins: [
     new ModuleFederationPlugin({
       name: "marketing",
+      filename: "remoteEntry.js",
       exposes: {
         "./MarketingApp": "./src/bootstrap.js",
       },
